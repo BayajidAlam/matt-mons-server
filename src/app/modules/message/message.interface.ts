e9077a59a5294c9122406a041144abcabd0ec5c5
BayajidAlam <bayajidalam2001@gmail.com>
2024-06-17 22:39:18 +0600

    service: message module created

--- src/app/modules/message/message.interface.ts
+++ src/app/modules/message/message.interface.ts
@@ -1,1 +1,7 @@
-// message interface
+export type IMessageFilters = {
+  searchTerm?: string | undefined;
+  message?: string | undefined;
+  senderId?: string | undefined;
+  receiverId?: string | undefined;
+  conversationId?: string | undefined;
+};
--- src/app/modules/message/message.route.ts
+++ src/app/modules/message/message.route.ts
@@ -1,1 +1,74 @@
-// message route
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import {
+  createMessage,
+  deleteSingleMessage,
+  getAllMessage,
+  getSingleMessage,
+  updateSingleMessage,
+} from './message.controller';
+import {
+  createMessageZodSchema,
+  updateMessageZodSchema,
+} from './message.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create-message',
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  //   ENUM_USER_ROLE.DRIVER,
+  //   ENUM_USER_ROLE.HELPER
+  // ),
+  validateRequest(createMessageZodSchema),
+  createMessage
+);
+
+router.get(
+  '/',
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  //   ENUM_USER_ROLE.DRIVER,
+  //   ENUM_USER_ROLE.HELPER
+  // ),
+  getAllMessage
+);
+
+router.get(
+  '/:id',
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  //   ENUM_USER_ROLE.DRIVER,
+  //   ENUM_USER_ROLE.HELPER
+  // ),
+  getSingleMessage
+);
+
+router.patch(
+  '/:id',
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  //   ENUM_USER_ROLE.DRIVER,
+  //   ENUM_USER_ROLE.HELPER
+  // ),
+  validateRequest(updateMessageZodSchema),
+  updateSingleMessage
+);
+
+router.delete(
+  '/:id',
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  //   ENUM_USER_ROLE.DRIVER,
+  //   ENUM_USER_ROLE.HELPER
+  // ),
+  deleteSingleMessage
+);
+
+export const MessageRoutes = router;
